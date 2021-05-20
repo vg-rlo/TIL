9f217b22f8abb305ccd7dc8b19fdd520892e32ad
gomprog <69677950+gomprog@users.noreply.github.com>
2021-05-20 23:57:49 +0900

    test case partly success

--- programmers_camouflage.py
+++ programmers_camouflage.py
@@ -1,5 +1,9 @@
 import math
 from itertools import product
+from functools import reduce
+
+def multiply(arr):
+        return reduce(lambda x, y: x * y, arr)
 
 def solution(clothes):
     answer = 0
@@ -7,18 +11,26 @@ def solution(clothes):
     kinds = list(set(list(zip(*clothes))[1])) # 옷 종류 
     clothes_dict = dict.fromkeys(kinds, [])
 
+    if len(kinds) == 1:
+        answer = len(clothes) 
+        print(answer)
+        return answer
+
     for i in clothes:
         clothes_dict[i[1]] = clothes_dict[i[1]] + [i[0]]
     # print('dict: ', clothes_dict)
     
-    answer = len(clothes_dict.keys()) # 조합의 수 구하기
+    clothes_len = [len(clothes_dict[c]) for c in clothes_dict]
+    print('length: ', clothes_len)
+
+    answer = len(clothes) + multiply(clothes_len)# 조합의 수 구하기
     return answer
 
 if __name__ == '__main__':
-    clothes = [
+    tests = [
         [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]],
         [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
         ] # 2차원 배열로 된 옷
 
-    result = [solution(c) for c in clothes]
+    result = [solution(test) for test in tests]
     print(result)
