75de65a5c0a2a6ddf182096120eac4890325d995
gomprog <69677950+gomprog@users.noreply.github.com>
2021-05-23 00:52:06 +0900

    success but time over

--- programmers_bignumber.py
+++ programmers_bignumber.py
@@ -1,3 +1,23 @@
-def solution(numbers):
+import itertools
+
+def solution(numbers: list):
     answer = ''
+    permuts = itertools.permutations(numbers, len(numbers))
+    permuts_numbers = []
+
+    for p in permuts:
+        tostring = [str(i) for i in p]
+        permuts_numbers.append("".join(tostring))
+
+    answer = max(permuts_numbers)
+
     return answer
+
+if __name__ == '__main__':
+    numbers = [
+        [6, 10, 2],         # result: 6210
+        [3, 30, 34, 5, 9]   # result: 9534330
+    ]
+
+    result = [solution(num) for num in numbers]
+    print(result)
