fca57a67967097cbd705348b35dbbb0124ec92d7
gomprog <69677950+gomprog@users.noreply.github.com>
2021-05-31 23:58:51 +0900

    fix timeover & add solution

--- programmers_scoville.py
+++ programmers_scoville.py
@@ -1,4 +1,43 @@
+# using heapq
 def solution(scoville: list, K: int):
+    '''
+    scoville: 스코빌 지수(맵기 정도) 
+    K: 임계값 
+    mixed_scoville: 최소와 2번째로 작은 스코빌 지수의 합
+    cnt_mixed: 섞은 회수
+    max_mixed: 섞은 후의 최대 scoville
+    '''
+    import heapq
+    import copy
+
+    answer = 0
+    cnt_mixed = 0
+    mixed_scoville = 0
+    scoville_copy = scoville.copy()
+
+    # greedy탐색법으로 maximum mixed scoville이 K보다 작으면 return -1
+    max_scoville = max(scoville_copy)
+    scoville_copy.pop(scoville_copy.index(max_scoville))
+    sec_scoville = max(scoville_copy)
+    max_mixed = max_scoville + sec_scoville 
+    if max_mixed < K:
+        return -1
+    
+    while(1):
+        # scoville내 원소가 모두 삭제거나 임계값 이상이면 break
+        if len(scoville) == 0 or mixed_scoville >= K:
+            break
+        
+        min_scoville = heapq.heappop(scoville)
+        sec_scoville = heapq.heappop(scoville)
+        
+        mixed_scoville = mixed_scoville + min_scoville + sec_scoville
+        cnt_mixed += 1
+
+    return cnt_mixed
+
+# using stack
+def timeover(scoville: list, K: int):
     '''
     scoville: 스코빌 지수(맵기 정도) 
     K: 임계값
@@ -31,3 +70,8 @@ def solution(scoville: list, K: int):
             # print(mixed_cnt)
             
         return mixed_cnt
+
+if __name__ == '__main__':
+    s = [1,2,3,9,10,12]
+    k = 7
+    print(solution(s, k))
