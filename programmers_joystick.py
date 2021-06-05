0b334ba77d81468f2c100aeb988a335a003799c6
vg-rlo <munorean@gmail.com>
2021-06-05 23:26:14 +0900

    add

---
+++ programmers_joystick.py
@@ -0,0 +1,11 @@
+def joystick(name):
+# def solution(name):
+    '''
+    ▲ - 다음 알파벳
+    ▼ - 이전 알파벳 (A에서 아래쪽으로 이동하면 Z로)
+    ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
+    ▶ - 커서를 오른쪽으로 이동
+    '''
+    answer = 0
+    
+    return answer
