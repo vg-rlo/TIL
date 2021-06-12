368f55726204301604fb4f0cb15aafd42e6bf382
gomprog <69677950+gomprog@users.noreply.github.com>
2021-06-12 23:47:19 +0900

    programmers greedy bignumber

---
+++ programmers_bignumbers.py
@@ -0,0 +1,10 @@
+def solution(number, k):
+    answer = ''
+    return answer
+
+if __name__ == '__main__':
+    nums = ['1924', '1231234', '4177252841']
+    k = [2, 3, 4]
+
+    for num, k_i in zip(nums):
+        print(solution(num, k_i))
