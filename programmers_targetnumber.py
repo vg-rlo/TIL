843206378d535c6c03e5ac55e3f94ea49111cd8e
gomprog <69677950+gomprog@users.noreply.github.com>
2021-07-02 10:53:02 +0900

    fix solution

--- programmers_targetnumber.py
+++ programmers_targetnumber.py
@@ -1,3 +1,23 @@
 def solution(numbers, target):
-    answer = 0
-    return answer
+    # answer = 0
+    cnt = 0
+     
+    for i in range(len(numbers)):
+        change_numbers = numbers
+        change_numbers[i] = -numbers[i]
+        s = sum(change_numbers)
+        print(change_numbers)
+
+        if s == target:
+            cnt += 1
+                
+    if sum(numbers) == target:
+        cnt += 1
+
+    return cnt
+
+if __name__ == '__main__':
+    nums = [1,1,1,1,1]
+    tgt = 3 
+    # answer = 5
+    print(solution(nums, tgt))
