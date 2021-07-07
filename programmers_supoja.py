6d5471f2b854a02595270a44d0c05ab66ecaf002
gomprog <69677950+gomprog@users.noreply.github.com>
2021-07-07 12:20:41 +0900

    fix solution
    필요없는 변수 사용 제거

--- programmers_supoja.py
+++ programmers_supoja.py
@@ -11,24 +11,20 @@ def solution(answers):
 
         # answers 길이 < spj 길이  
         if quot == 0:
-            ext_spj = spj[:len(answers)]    
+            extnd_spj = spj[:len(answers)]    
         # answers 길이 >= spj 길이  
         else:
-            ext_spj = spj + (quot-1)*spj # 배수만큼 연장
-            ext_remain = len(answers) % len(ext_spj)
-            if ext_remain != 0:
-                ext_spj = ext_spj + spj[:ext_remain] # 나머지만큼 연장
-        # print("spj: ", ext_spj, len(ext_spj))
+            extnd_spj = spj + (quot-1)*spj # 배수만큼 연장
+            remain = len(answers) % len(extnd_spj)
+            if remain != 0:
+                extnd_spj = extnd_spj + spj[:remain] # 나머지만큼 연장
+        # print("spj: ", extnd_spj, len(extnd_spj))
         # print()
                     
-        # 각 원소끼리 정답 원소와 비교 
-        ext_arr = np.array(ext_spj)
-        ans_arr = np.array(answers)
-        # print(ext_arr, '=>', len(ext_arr))
-        # print(ans_arr, '=>', len(ans_arr))
-        
-        ans = ans_arr[ext_arr == ans_arr]
-        correct.append(len(ans))
+        # 각 원소끼리 정답 원소와 비교       
+        ans_arr = np.array(answers)  
+        ans_arr = ans_arr[np.array(extnd_spj) == ans_arr]
+        correct.append(len(ans_arr))
         # print(correct)
 
     correct_arr = np.array(correct)
