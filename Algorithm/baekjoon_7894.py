8b3bd99bf706ab374527f1d681426d8bf98061c0
vg-rlo <munorean@gmail.com>
2021-10-19 16:30:17 +0900

    baekjoon 7894 for loop

--- Algorithm/baekjoon_7894.py
+++ Algorithm/baekjoon_7894.py
@@ -1,7 +1,8 @@
-# 재귀 알고리즘
+# 재귀 알고리즘 대신 단순 for loop
 # 수학식 활용 - log
-# Time over
+# Time over solved
 # 참고 블로그 - https://prod.velog.io/@inwooleeme/%EB%B0%B1%EC%A4%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-7894%EB%B2%88-%ED%81%B0-%EC%88%98
+# 참고 블로그 - https://lifedev0.tistory.com/78
 import sys # 입력
 from math import factorial, log10, ceil # 팩토리얼, 상용로그, 올림
 
@@ -20,22 +21,26 @@ from math import factorial, log10, ceil # 팩토리얼, 상용로그, 올림
 #         digit += 1
 #     return digit
 
-def calc_digit(n):
-    if n == 1:
-        return log10(1)
-    return log10(n)+calc_digit(n-1)
+# def calc_digit(n):
+#     if n == 1:
+#         return log10(1)
+#     return log10(n)+calc_digit(n-1)
 
 
 if __name__ == "__main__":
     num = int(sys.stdin.readline())
     # num = int(input())
-    num_list = []
-
     for i in range(num):
-        num_list.append(int(sys.stdin.readline()))
-    
-    for n in num_list:
-        # Time over
-        # print(calc_digit(factorial(n)))
-        # print(ceil(log10(factorial(n))))
-        print(ceil(calc_digit(n)))
+        log_sum = 0
+        n = int(sys.stdin.readline())
+        for i in range(1, n+1):
+            log_sum += log10(i)
+        print(int(log_sum)+1)
+        
+        # Wrong
+        # print(ceil(log_sum)) 
+
+    # Time over
+    # for n in num_list:
+    #     print(calc_digit(factorial(n)))
+    #     print(ceil(log10(factorial(n))))
